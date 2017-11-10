12f601955bac1c36e3f3267b25b0b06435d0bf6c
Kevin Rodriguez <krodriguez@emida.net>
2017-11-10 11:16:59 -0500

    Add a line Info Error or Succes

--- src/main/java/com/images/HomeController.java
+++ src/main/java/com/images/HomeController.java
@@ -2,41 +2,24 @@ package com.images;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLConnection;
+
 
 /**
- * @author Franky Villadiego
+ * @author Kevin Rodriguez
  */
-
 @Controller
 public class HomeController {
-
 
     private String imageDir;
 
     private static final Logger log = LoggerFactory.getLogger(HomeController.class);
-
-
-    //private ImageService imageService;
-
-/*    @GetMapping("/")
-    public String home(){
-        return "home";
-    }*/
-
-
     @GetMapping("/**")
     public void images(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
@@ -49,19 +32,18 @@ public class HomeController {
 
             File file = new File(fullPath);
             if (file.exists() && file.isFile()) {
-                log.info("Exist...");
+                log.info("Exist the Image > URL> ... " + file.toString());
                 ImageOptimizer.sendImage(req, resp, file);
             } else {
                 //search in DB
-                    System.out.println("ERROR");
-                }         
+                log.info("it is not possible to access the image (the most certain thing is that it does not exist.!!!!!)");
+            }
 
         } catch (Exception ex) {
             log.error("Error : {}", ex);
         }
 
     }
-
 
     @Value("${image.dir}")
     public void setImageDir(String imageDir) {
